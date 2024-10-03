1a80099fa5b3ecafd2c6c10f2981222c111d29e2
Mit Bailey <mitbailey@outlook.com>
2024-10-03 15:06:31 -0400

    Added basic packet structure.

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,16 @@ use serde::{Serialize, Deserialize};
 // Packet
 #[derive(Serialize, Deserialize, Debug)]
 struct GenCamPacket {
-
+    packet_type: PacketType,
+    packet_id: u32,
+    data: Vec<u8>,
+    crc: u32,
 }
 
 // Comms type
+#[derive(Serialize, Deserialize, Debug)]
 enum PacketType {
-    Acknowledged,
-    NotAcknowledged,
+    Ack,
+    NAck,
     Data,
-    Image,
 }
